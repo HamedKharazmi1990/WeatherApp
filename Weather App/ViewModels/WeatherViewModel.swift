efbc2fe997d81153a6299fd37182f1a2f922a395
hamed kharazmi <hamed.kharazmi@bandpey.com>
2025-12-29 18:37:03 +0100

    fetch weather data using url session and async await

--- Weather App/ViewModels/WeatherViewModel.swift
+++ Weather App/ViewModels/WeatherViewModel.swift
@@ -17,24 +17,29 @@ class WeatherViewModel {
     private let apiKey = "APIKEY"
     
     private func fetchWeather(for city: String) async throws -> WeatherResponse {
+        // Build URL
         let urlString = "http://api.weatherapi.com/v1/current.json?key=\(apiKey)&q=\(city)&aqi=no"
+        guard let url = URL(string: urlString) else {
+            throw WeatherError.invalidURL
+        }
         
+        // Fetch data
+        let (data, response) = try await URLSession.shared.data(from: url)
         
+        // Validate response
+        guard let httpResponse = response as? HTTPURLResponse else {
+            throw WeatherError.unknown
+        }
         
+        guard httpResponse.statusCode == 200 else {
+            throw WeatherError.requestFailed(statusCode: httpResponse.statusCode)
+        }
         
-        return .init(
-            location: .init(
-                name: "London",
-                country: "England"
-            ),
-            current: .init(
-                tempC: 30,
-                condition: .init(
-                    text: "Great Condition",
-                    icon: "Icon Name"
-                ),
-                feelsLikeC: 32.5
-            )
-        )
+        // Decode Model
+        do {
+            return try JSONDecoder().decode(WeatherResponse.self, from: data)
+        } catch {
+            throw WeatherError.decodingFailed
+        }
     }
 }
