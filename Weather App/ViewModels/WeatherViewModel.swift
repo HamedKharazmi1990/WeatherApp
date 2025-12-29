3588a7b8800c880903ebeddbf965b53c37dc62c5
hamed kharazmi <hamed.kharazmi@bandpey.com>
2025-12-29 18:15:07 +0100

    Set url string for api call

--- Weather App/ViewModels/WeatherViewModel.swift
+++ Weather App/ViewModels/WeatherViewModel.swift
@@ -15,4 +15,26 @@ class WeatherViewModel {
     var errorMessage: String?
     
     private let apiKey = "APIKEY"
+    
+    private func fetchWeather(for city: String) async throws -> WeatherResponse {
+        let urlString = "http://api.weatherapi.com/v1/current.json?key=\(apiKey)&q=\(city)&aqi=no"
+        
+        
+        
+        
+        return .init(
+            location: .init(
+                name: "London",
+                country: "England"
+            ),
+            current: .init(
+                tempC: 30,
+                condition: .init(
+                    text: "Great Condition",
+                    icon: "Icon Name"
+                ),
+                feelsLikeC: 32.5
+            )
+        )
+    }
 }
